6a09e1228a41457ade6a677135b2055fb2ffd2f0
Ruben Frias <rubenfg2003@gmail.com>
2024-04-01 10:05:22 -0400

    hfngvhb

--- src/main/java/com/example/edfinal/RandomFeaturesPicker.java
+++ src/main/java/com/example/edfinal/RandomFeaturesPicker.java
@@ -1,4 +1,49 @@
 package com.example.edfinal;
 
+import java.util.Random;
+
 public class RandomFeaturesPicker {
+//                  Min  Max
+//    sepal length: 4.3  7.9
+//    sepal width:  2.0  4.4
+//    petal length: 1.0  6.9
+//    petal width:  0.1  2.5
+
+    private static double sepalLengthMin = 4.3;
+    private static double sepalLengthMax = 7.9;
+    private static double sepalWidthMin = 2.0;
+    private static double sepalWidthMax = 4.4;
+
+    private static double petalLengthMin = 1.0;
+    private static double petalLengthMax = 6.9;
+    private static double petalWidthMin = 0.1;
+    private static double petalWidthMax = 2.5;
+
+    public static double randomSepalLength()
+    {
+        Random rand = new Random();
+
+        return sepalLengthMin + (sepalLengthMax-sepalLengthMin) * rand.nextDouble();
+    }
+
+    public static double randomSepalWidth()
+    {
+        Random rand = new Random();
+
+        return sepalWidthMin + (sepalWidthMax-sepalWidthMin) * rand.nextDouble();
+    }
+
+    public static double randomPetalLength()
+    {
+        Random rand = new Random();
+
+        return petalLengthMin + (petalLengthMax-petalLengthMin) * rand.nextDouble();
+    }
+
+    public static double randomPetalWidth()
+    {
+        Random rand = new Random();
+
+        return petalWidthMin + (petalWidthMax-petalWidthMin) * rand.nextDouble();
+    }
 }
