343bdf369380af77f77cd6b3864bb668dba94157
Ruben Frias <rubenfg2003@gmail.com>
2024-04-01 10:07:04 -0400

    hfngvhb

--- src/main/java/com/example/edfinal/SOMNeuron.java
+++ src/main/java/com/example/edfinal/SOMNeuron.java
@@ -1,4 +1,22 @@
 package com.example.edfinal;
 
 public class SOMNeuron {
+
+    private Flower flower;
+
+    public SOMNeuron()
+    {
+        this.flower= new Flower();
+    }
+
+    public double euclidianDistance(Flower flower)
+    {
+        double petalLengthDist = Math.pow((this.flower.getPetalLength()-flower.getPetalLength()),2);
+        double petalWidthDist = Math.pow((this.flower.getPetalWidth()-flower.getPetalWidth()),2);
+        double sepalLengthDist = Math.pow((this.flower.getSepalLength()-flower.getSepalLength()),2);
+        double sepalWidthDist = Math.pow((this.flower.getSepalWidth()-flower.getSepalWidth()),2);
+
+        return Math.sqrt( petalLengthDist +  petalWidthDist + sepalLengthDist + sepalWidthDist);
+    }
+
 }
