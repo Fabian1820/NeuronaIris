3f118f7348327518bc89275d71fe7f30ef3951ae
Ruben Frias <rubenfg2003@gmail.com>
2024-04-01 10:06:31 -0400

    hfngvhb

--- src/main/java/com/example/edfinal/SOM.java
+++ src/main/java/com/example/edfinal/SOM.java
@@ -1,4 +1,18 @@
 package com.example.edfinal;
+import java.util.ArrayList;
 
 public class SOM {
+    private ArrayList<SOMNeuron> neurons;
+
+//    public SOMNeuron findBMU(Flower flower)
+//    {
+//
+//    }
+
+    public ArrayList<SOMNeuron> getNeurons()
+    {
+        return this.neurons;
+    }
+
+
 }
