eada527b5a654db1dd7141ce66708665558551a4
Ruben Frias <rubenfg2003@gmail.com>
2024-04-01 10:04:27 -0400

    hfngvhb

--- src/main/java/com/example/edfinal/Flower.java
+++ src/main/java/com/example/edfinal/Flower.java
@@ -1,4 +1,64 @@
 package com.example.edfinal;
 
+import java.util.Random;
+
 public class Flower {
+    private double petalWidth;
+    private double petalLength;
+    private double sepalWidth;
+    private double sepalLength;
+
+    public Flower(String type, double petalWidth, double petalLength, double sepalWidth, double sepalLength)
+    {
+
+        setPetalWidth(petalWidth);
+        setPetalLength(petalLength);
+        setSepalWidth(sepalWidth);
+        setSepalLength(sepalLength);
+    }
+
+    public Flower()
+    {
+        this.petalWidth=RandomFeaturesPicker.randomPetalWidth();
+        this.petalLength=RandomFeaturesPicker.randomPetalLength();
+        this.sepalLength=RandomFeaturesPicker.randomSepalLength();
+        this.sepalWidth=RandomFeaturesPicker.randomSepalWidth();
+    }
+
+    private void setSepalLength(double sepalLength) {
+        //validaciones luego
+        this.sepalLength=sepalLength;
+    }
+
+    private void setSepalWidth(double sepalWidth) {
+        //validaciones luego
+        this.sepalWidth=sepalWidth;
+    }
+
+    private void setPetalLength(double petalLength) {
+        //validaciones luego
+        this.petalLength=petalLength;
+    }
+
+    private void setPetalWidth(double petalWidth) {
+        //validaciones luego
+        this.petalWidth=petalWidth;
+    }
+
+    public double getPetalWidth() {
+        return this.petalWidth;
+    }
+
+    public double getPetalLength() {
+        return this.petalLength;
+    }
+
+    public double getSepalWidth() {
+        return this.sepalWidth;
+    }
+
+    public double getSepalLength() {
+        return this.sepalLength;
+    }
+
 }
