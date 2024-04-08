352d4c23ad99a0577506935be976bf1bcddf3646
Clari21 <molesClari21@gmail.com>
2024-04-07 17:03:56 -0800

    aqui en la guara

--- src/main/java/com/example/edfinal/HelloController.java
+++ src/main/java/com/example/edfinal/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.edfinal;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
